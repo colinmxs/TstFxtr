7857eb1f2a080e9df63e8891e2a06c8b60e699f5
Colin Smith <smith.colin00@gmail.com>
2017-07-02 22:47:22 -0600

    Setup testing infrastructure and tweaked customization api (#2)
    * created some anemic models to test create function XD
    
    * wip
    
    * added .sln to the repo
    
    * pass functions as customizations
    
    * added tests and changed up customizaation api
    
    * tweaks to fix build
    
    * testing a small change
    
    * tweakin it back

---
+++ tests/TstFxtr.Tests/GenFxtr.cs
@@ -0,0 +1,85 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using static TstFxtr.GenFxtr;
+
+namespace TstFxtr.Tests
+{
+    [TestClass]
+    public class GenFxtr
+    {
+        [TestMethod]
+        public void DoesTheJob()
+        {
+            var customer = Create<Customer>();
+            Assert.IsNotNull(customer);
+            Assert.IsNotNull(customer.Person);
+            Assert.IsNotNull(customer.Person.Address);
+            Assert.IsNotNull(customer.Person.Address.Street);
+            Assert.IsNotNull(customer.Person.Address.City);
+            Assert.IsNotNull(customer.Person.Address.State);
+            Assert.IsNotNull(customer.Person.Address.Zip);
+            Assert.IsNotNull(customer.Person.Address.Country);
+            Assert.IsNotNull(customer.Person.Birthday);
+            Assert.IsNotNull(customer.Person.ContactInfo);
+            Assert.IsNotNull(customer.Person.ContactInfo.Email);
+            Assert.IsNotNull(customer.Person.ContactInfo.Phone);
+            Assert.IsNotNull(customer.Person.FirstName);
+            Assert.IsNotNull(customer.Person.LastName);
+            Assert.IsNotNull(customer.Orders);
+
+            var orders = Create<Order>();
+            Assert.IsNotNull(orders.Products);
+            Assert.IsNotNull(orders.Products[0]);
+            Assert.IsNotNull(orders.Products[0].Id);
+            Assert.IsNotNull(orders.Products[0].Name);
+            Assert.IsNotNull(orders.Store);
+            Assert.IsNotNull(orders.Store.Location);
+            Assert.IsNotNull(orders.Store.Manager);
+            Assert.IsNotNull(orders.Store.Manager.Person);
+            Assert.IsNotNull(orders.Store.Manager.Person);
+            Assert.IsNotNull(orders.Store.Manager.Person.Address);
+            Assert.IsNotNull(orders.Store.Manager.Person.Address.Street);
+            Assert.IsNotNull(orders.Store.Manager.Person.Address.City);
+            Assert.IsNotNull(orders.Store.Manager.Person.Address.State);
+            Assert.IsNotNull(orders.Store.Manager.Person.Address.Zip);
+            Assert.IsNotNull(orders.Store.Manager.Person.Address.Country);
+            Assert.IsNotNull(orders.Store.Manager.Person.Birthday);
+            Assert.IsNotNull(orders.Store.Manager.Person.ContactInfo);
+            Assert.IsNotNull(orders.Store.Manager.Person.ContactInfo.Email);
+            Assert.IsNotNull(orders.Store.Manager.Person.ContactInfo.Phone);
+            Assert.IsNotNull(orders.Store.Manager.Person.FirstName);
+            Assert.IsNotNull(orders.Store.Manager.Person.LastName);
+            Assert.IsNotNull(orders.Store.Number);
+            Assert.IsNotNull(orders.Timestamp);            
+        }
+
+        [TestMethod]
+        public void ConstructorCustomizationExistingObject()
+        {
+            var person = Create<Person>();
+            var customization = Customize(typeof(Customer))
+                .ConstructorParams(person);
+            _generator.Customize(customization);
+            var customer = Create<Customer>();
+            Assert.AreSame(person, customer.Person);
+        }
+
+        [TestMethod]
+        public void ConstructorCustomizationFunc()
+        {
+            var customization = Customize(typeof(Customer))
+                .ConstructorFuncs(new Func<Person>(() => Create<Person>()));
+            _generator.Customize(customization);
+            var cust1 = Create<Customer>();
+            var cust2 = Create<Customer>();
+            Assert.AreNotSame(cust1.Person, cust2.Person);
+        }
+        
+        //[TestMethod]
+        //public void ValueTypePropertyCustomizations()
+        //{
+        //    var firstName = "Johnny";
+        //    Customize(typeof(string)).AnyProperties(property => property.Contains("FirstName"), firstName);
+        //}
+    }
+}
