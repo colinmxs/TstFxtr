a1b246e443960985451cdf2b70969e7d379f50a6
Colin Smith <ColinSmith1@boisestate.edu>
2018-07-26 10:20:49 -0600

    fix LoremGenerator wordbank init broken by codenamegenerator update

--- src/TstFxtr/GeneratorWordBanks.cs
+++ src/TstFxtr/GeneratorWordBanks.cs
@@ -4,6 +4,6 @@ namespace TstFxtr
 {
     public class GeneratorWordBanks
     {
-        public static WordBank LoremWordBank = new WordBank(Word.Noun, "Lorems", new WordRepository(LoremConstants.Lorem1, LoremConstants.Hairy, LoremConstants.Web2, LoremConstants.Hipster, LoremConstants.Fillerama, LoremConstants.Arrested, LoremConstants.Bacon, LoremConstants.Cupcake, LoremConstants.Beer, LoremConstants.Samuel, LoremConstants.Corporate, LoremConstants.Fillerati, LoremConstants.Zombie, LoremConstants.Gangster, LoremConstants.Journo, LoremConstants.Cat, LoremConstants.Veggie, LoremConstants.Picksum, LoremConstants.Hodor));
+        public static WordBank LoremWordBank = new WordBank(Word.Noun, "Lorems", new WordRepository(new string[] { LoremConstants.Lorem1, LoremConstants.Hairy, LoremConstants.Web2, LoremConstants.Hipster, LoremConstants.Fillerama, LoremConstants.Arrested, LoremConstants.Bacon, LoremConstants.Cupcake, LoremConstants.Beer, LoremConstants.Samuel, LoremConstants.Corporate, LoremConstants.Fillerati, LoremConstants.Zombie, LoremConstants.Gangster, LoremConstants.Journo, LoremConstants.Cat, LoremConstants.Veggie, LoremConstants.Picksum, LoremConstants.Hodor }));
     }
 }
