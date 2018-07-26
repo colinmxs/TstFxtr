2033df9c934efb7b178d7c4e036c2f3bb5a1b88c
Colin Smith <ColinSmith1@boisestate.edu>
2018-07-26 10:15:13 -0600

    add GeneratorWordBanks.cs and implement LoremWordBank field

---
+++ src/TstFxtr/GeneratorWordBanks.cs
@@ -0,0 +1,9 @@
+﻿using CodenameGenerator;
+
+namespace TstFxtr
+{
+    public class GeneratorWordBanks
+    {
+        public static WordBank LoremWordBank = new WordBank(Word.Noun, "Lorems", new WordRepository(LoremConstants.Lorem1, LoremConstants.Hairy, LoremConstants.Web2, LoremConstants.Hipster, LoremConstants.Fillerama, LoremConstants.Arrested, LoremConstants.Bacon, LoremConstants.Cupcake, LoremConstants.Beer, LoremConstants.Samuel, LoremConstants.Corporate, LoremConstants.Fillerati, LoremConstants.Zombie, LoremConstants.Gangster, LoremConstants.Journo, LoremConstants.Cat, LoremConstants.Veggie, LoremConstants.Picksum, LoremConstants.Hodor));
+    }
+}
