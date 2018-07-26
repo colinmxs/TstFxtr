76be06baf719476b270c52002cd8a677e9406aff
Colin Smith <ColinSmith1@boisestate.edu>
2018-07-26 10:17:23 -0600

    add Generators.cs and implement LoremGenerator, NameGenerator, and LocationGenerator

---
+++ src/TstFxtr/Generators.cs
@@ -0,0 +1,32 @@
+﻿using CodenameGenerator;
+
+namespace TstFxtr
+{
+    public class Generators
+    {
+        public static Generator LoremGenerator
+        {
+            get
+            {
+                return new Generator(" ", Casing.LowerCase, GeneratorWordBanks.LoremWordBank);
+            }
+        }
+        
+        public static Generator NameGenerator
+        {
+            get
+            {
+                var nameGenerator = new Generator(" ", Casing.PascalCase, WordBank.FirstNames, WordBank.LastNames);
+                return nameGenerator;
+            }
+        }
+
+        public static Generator LocationGenerator
+        {
+            get
+            {
+                return new Generator(", ", Casing.PascalCase, WordBank.Cities, WordBank.StateNames);
+            }
+        }
+    }
+}
